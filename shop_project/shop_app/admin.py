abc519c1aa563c0dd66bb64701fdcb54aa0e49d3
David <toled@MacBook-Air-de-David.local>
2018-12-18 16:53:37 +0200

    Ajout de questions reponses

--- shop_project/shop_app/admin.py
+++ shop_project/shop_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from shop_app.models import Product, Client, Maillot, Comment
+from shop_app.models import Product, Client, Maillot, Comment, Question, Response
 
 admin.site.register(Product)
 admin.site.register(Client)
 admin.site.register(Maillot)
 admin.site.register(Comment)
+admin.site.register(Question)
+admin.site.register(Response)
--- shop_project/shop_app/forms.py
+++ shop_project/shop_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from shop_app.models import Comment
+from shop_app.models import Comment, Question, Response
 
 class CommentForm(forms.ModelForm):
 	class Meta:
@@ -17,4 +17,42 @@ class CommentForm(forms.ModelForm):
 			'required': True
 			})
 		}
-		
+
+class QuestionForm(forms.ModelForm):
+	class Meta:
+		model = Question
+		fields = ['title', 'username', 'text']
+		widgets = {
+		'username': forms.TextInput(attrs={
+			'id':'comment-username',
+			'placeholder':'username',
+			'required': True
+			}),
+		'title': forms.TextInput(attrs={
+			'id':'comment-title',
+			'placeholder':'title',
+			'required': True
+			}),
+		'text': forms.Textarea(attrs={
+			'id':'comment-text',
+			'placeholder':'text',
+			'required': True
+			})
+		}
+
+class ResponseForm(forms.ModelForm):
+	class Meta:
+		model = Response
+		fields = ['username', 'text']
+		widgets = {
+		'username': forms.TextInput(attrs={
+			'id':'comment-username',
+			'placeholder':'username',
+			'required': True
+			}),
+		'text': forms.Textarea(attrs={
+			'id':'comment-text',
+			'placeholder':'text',
+			'required': True
+			})
+		}
